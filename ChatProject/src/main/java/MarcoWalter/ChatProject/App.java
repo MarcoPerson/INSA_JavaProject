3e59f7ea083ff22ba1215799ec2f46c1c59ac4dc
Merveilles AGBETI-MESSAN <marcoperson2000@gmail.com>
2022-12-12 15:50:30 +0100

    Test : Assemblable UDP

--- ChatProject/src/main/java/MarcoWalter/ChatProject/App.java
+++ ChatProject/src/main/java/MarcoWalter/ChatProject/App.java
@@ -7,6 +7,10 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Scanner;
+
+import MarcoWalter.ChatProject.Models.User;
+import MarcoWalter.ChatProject.UdpControllers.UserSocketUDP;
 
 /**
  * JavaFX App
@@ -32,7 +36,27 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-    	
+    	try {
+    		User walter = new User(1);
+            Scanner pseudoChooser = new Scanner(System.in);
+            UserSocketUDP walSocketUDP = new UserSocketUDP(walter);
+            boolean agreed = false;
+            do {
+                System.out.print("Choose a new Pseudo : ");
+                walter.modifyPseudo(pseudoChooser.nextLine());
+                walter.connectToNetwork(walSocketUDP);
+                agreed = walSocketUDP.waitForAggrement();
+            } while (agreed == false);
+
+            walSocketUDP.broadcast(walter.getId(), walter.getPseudo(), "newUser");
+            Thread reception = new Thread(() -> walSocketUDP.receiveMessage());
+            reception.start();
+
+            pseudoChooser.close();
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    	}
         launch();
     }
 
